7158edd327713aedf28cc6d50b3013509f1bcc13
laipz8200 <laipz8200@icloud.com>
2018-09-26 12:57:45 +0800

    update

--- Crawler/utils.py
+++ Crawler/utils.py
@@ -12,7 +12,8 @@ def get_page(url):
     :return: 网页源码
     """
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/69.0.3497.100 Safari/537.36',
     }  # 请求头信息
     res = requests.get('http://localhost:5555/random')
     proxies = {
